bd8feb248e159b3a40d7c375746bd1843790ea19
faruklmu17 <41293321+faruklmu17@users.noreply.github.com>
2024-12-02 17:18:23 -0500

    Update playwright.py

--- playwright.py
+++ playwright.py
@@ -1,1 +1,23 @@
+from playwright.sync_api import sync_playwright
+
+# This is the main function that runs the script
+def run():
+    with sync_playwright() as p:
+        
+        browser = p.chromium.launch(headless=False) 
+        page = browser.new_page()
+        
+        # Navigate to a webpage
+        # change it to google
+        page.goto('https://example.com')
+        
+        # Take a screenshot of the page
+        page.screenshot(path='test.png')
+        
+        # Close the browser
+        browser.close()
+
+# Call the function to execute
+if __name__ == '__main__':
+    run()
 
